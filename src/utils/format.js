df9e698b298aa7156b8586bd925dd4f61f52079a
Iqbal Syahbana <iqbalsyahbana14@gmail.com>
2025-09-15 16:32:28 +0700

    update date format in formatDate function to use spaces instead of dashes

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const formatDate = (date) => moment(date).format("DD-MM-YYYY");
+export const formatDate = (date) => moment(date).format("DD MMM YYYY");
 
 export const formatCurrency = (value) =>
   new Intl.NumberFormat("id-ID", {
