cbcc43f8a6865cdf43c0252b8c2fe998a725b30d
Iqbal Syahbana <iqbalsyahbana14@gmail.com>
2025-10-01 17:06:46 +0700

    refactor API URL configuration to use environment variable; improve flexibility for different environments

--- src/api/saldoApi.js
+++ src/api/saldoApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/saldo";
+const API_URL = process.env.REACT_APP_API_URL + "/api/saldo";
 
 export const getAllSaldo = async (params = {}) => {
   const res = await axios.get(API_URL, { params });
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users";
+const API_URL = process.env.REACT_APP_API_URL + "/api/users";
 
 export const getAllUsers = async () => {
   const res = await axios.get(API_URL);
